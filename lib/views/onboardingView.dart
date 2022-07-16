5bd5cb8c8250ad8f2ca87794a489ba5aad37fc34
Telson kapassolo <telsonkapassolo@MBP-de-Telson.home>
2022-07-16 21:56:42 +0100

    moving

---
+++ lib/views/onboardingView.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  OnBoardingPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  List<String> myCareer = ['Geral','Cardiovascular','Cabeça e pescoço','Torácica','Ortopédica'];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      
+    );
+  }
+
+}
